0f1293e1cd6f35580a2f5f40978c02dadc844c97
Emilian Con <emiliancon@Emilians-MacBook-Pro.local>
2022-07-09 09:45:39 +0100

    prod.py update

--- zinc/settings/prod.py
+++ zinc/settings/prod.py
@@ -11,4 +11,39 @@ ALLOWED_HOSTS = [
     'idea-development.herokuapp.com', 'www.ideadev.co.uk', 'ideadev.co.uk'
 ]
 
+# Server Logging
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+
+
+# Heroku Settings
 django_on_heroku.settings(locals())
+
+del DATABASES['default']['OPTIONS']['sslmode']
